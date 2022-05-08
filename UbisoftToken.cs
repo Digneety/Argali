fb34ac976c9737d1dca08d7de7383dc31b7b9702
Jan <eleventep@gmail.com>
2022-05-08 16:13:19 +0200

    Changed Icon

--- UbisoftToken.cs
+++ UbisoftToken.cs
@@ -5,6 +5,7 @@ namespace odk
     public class UbisoftToken
     {
         public string Ticket { get; set; }
+
         public DateTime Expiration { get; set; }
 
         public bool IsExpired => Expiration <= DateTime.Now;
